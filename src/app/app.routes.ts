46da9f8b0015544c44475cb4b3b5be45b6a314ae
srdgz <sandra.rr@icloud.com>
2024-10-11 10:28:54 +0200

    feat: add new view to add product

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductoListaComponent } from './producto-lista/producto-lista.component';
+import { AgregarProductoComponent } from './agregar-producto/agregar-producto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'productos', component: ProductoListaComponent },
+  { path: '', redirectTo: 'productos', pathMatch: 'full' },
+  { path: 'add', component: AgregarProductoComponent },
+];
